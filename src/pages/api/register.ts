2cf36be22dc843bc89cae62ddff4e48d17ce6b32
Sidhesh-nishad <155049903+Sidhesh-nishad@users.noreply.github.com>
2024-03-16 12:04:43 +0530

    bug fixes

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -3,17 +3,22 @@ import type { APIRoute } from "astro";
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
 
-  data.append("Date_Time", new Date().toLocaleString('en-GB', { timeZone: 'IST' }));
+  data.append(
+    "Date_Time",
+    new Date().toLocaleString("en-GB", { timeZone: "IST" })
+  );
 
-  const response = await fetch('https://script.google.com/macros/s/AKfycbxmeq26ZUV16tFvhFrQoqPEd5LdcovwpIlwzBP_moJFKkG5Q-qgfk3Dxs2MNg6MFrDB/exec', {
-    method: 'POST',
-    body: data
-  });
+  const response = await fetch(
+    "https://script.google.com/macros/s/AKfycbxnAzKs9TOK7JIpetPoCvUgzOIdO-5d8l9BT5ogHaOeuzLXIeBZcQU3I2-TIbRHVWhC/exec",
+    {
+      method: "POST",
+      body: data,
+    }
+  );
 
-  const res =  await response.json()
+  const res = await response.json();
 
-
-  console.log('res...', res)
+  console.log("res...", res);
 
   // Validate the data - you'll probably want to do more than this
   // if (!name || !email || !message) {
@@ -27,7 +32,7 @@ export const POST: APIRoute = async ({ request }) => {
   // // Do something with the data, then return a success response
   return new Response(
     JSON.stringify({
-      message: "Success hit!"
+      message: "Success hit!",
     }),
     { status: 200 }
   );
